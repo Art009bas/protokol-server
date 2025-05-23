c6cc63255644c6b6b7e5bf2c4b98d044f5194deb
Art009bas <ipbasaev@mail.ru>
2025-05-23 13:17:38 +0700

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import authRoutes from './routes/auth.routes.js';
 import meetingRoutes from './routes/meeting.routes.js';
 import taskRoutes from './routes/task.routes.js';
 import userRoutes from './routes/user.routes.js'; // ✅ импортируем
+import publicTaskRoutes from './routes/publicTask.routes.js';
 
 import { verifyToken } from './middleware/auth.middleware.js';
 
@@ -15,6 +16,7 @@ const app = express(); // ✅ создаём app
 
 const prisma = new PrismaClient();
 
+app.use('/public-tasks', publicTaskRoutes);
 app.use(cors());
 app.use(express.json());
 
